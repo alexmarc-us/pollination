6728135c22f2e2d7c661e3631bed8199c03581f6
Alex Marcus <mr@alexmarc.us>
2025-05-06 08:32:36 -0700

    Update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://alexmarc-us.github.io",
-    base: "pollination",
+    base: "/pollination",
 });
