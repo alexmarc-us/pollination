6b564c1806822b7ec7198088944efa0ce28195dd
Alex Marcus <mr@alexmarc.us>
2025-05-14 21:10:42 -0700

    Minimal start

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+<<<<<<< HEAD
 export default defineConfig({
     site: "https://alexmarc-us.github.io",
     base: "/pollination",
 });
+=======
+export default defineConfig({});
+>>>>>>> 689674d (Minimal start)
